5a4927cec991794512426414060772c85ed3d29b
scscdslt <542616325@qq.com>
2020-04-30 17:42:05 +0800

    添加users模块

---
+++ cloudbackend-parent/cloudbackend-user/src/main/java/com/lt/cloud/cloud/user/CloudbackendUserApplication.java
@@ -0,0 +1,13 @@
+package com.lt.cloud.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbackendUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudbackendUserApplication.class, args);
+    }
+
+}
---
+++ cloudbackend-parent/cloudbackend-user/src/test/java/com/lt/cloud/cloud/user/CloudbackendUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.cloud.cloud.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbackendUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
